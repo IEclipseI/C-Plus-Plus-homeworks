e43bda5b41812047bf6a718d051e5f4c170d9bd7
IEclipseI <38165416+IEclipseI@users.noreply.github.com>
2018-05-19 21:16:35 +0300

    Update op_vector.h

--- HW2/op_vector/op_vector.h
+++ HW2/op_vector/op_vector.h
@@ -8,26 +8,23 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <cstring>
 using namespace std;
+
+static const size_t _small_object_size = 2;
+
 
 class op_vector {
  public:
-  op_vector(const op_vector &) noexcept;
-  op_vector &operator=(const op_vector &) noexcept;
   op_vector();
+  op_vector(const op_vector &) noexcept;
   explicit op_vector(size_t size);
+  op_vector &operator=(const op_vector &) noexcept;
+  ~op_vector();
  private:
-  size_t _size;
-  bool is_small;
-  static const size_t _small_object_size = 1;
-  size_t _capacity;
-  uint32_t small_object_data[_small_object_size];
-  shared_ptr<vector<uint32_t>> _big_object_ptr;
-  uint32_t *data_ptr;
- private:
+  bool _is_small() const;
   void ensure_capacity(size_t size);
  public:
-  size_t small_object_size() const;
   size_t capacity() const;
   size_t size() const;
   void push_back(uint32_t elem);
@@ -36,10 +33,19 @@ class op_vector {
   void resize(uint32_t size);
   void make_unique();
   void pop_back();
-  const shared_ptr<vector<uint32_t>>& big_object_ptr();
   uint32_t back();
   void inverse_values();
   void swap(op_vector& rhs);
+ private:
+  uint32_t *data_ptr;
+  union data {
+    uint32_t small_object_data[_small_object_size];
+    shared_ptr<vector<uint32_t>> _big_object_ptr;
+    data(){}
+    ~data(){}
+  } u;
+  size_t _size;
+  size_t _capacity;
 };
 
 #endif //VECTOR_OPTIMIZATIONS_OPTIMIZED_VECTOR_H
